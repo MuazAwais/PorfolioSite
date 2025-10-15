c141ce5dbe2378456fd943bc54b43f2155728b92
muazawais <muazawais1947@gmail.com>
2025-10-15 18:15:27 +0500

    protfolio posting

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -27,23 +27,19 @@ const sitemap = [
 const socials = [
   {
     label: 'GitHub',
-    href: 'https://www.github.com/codewithsadee-org'
+    href: 'https://www.github.com/muazawais'
   },
   {
     label: 'LinkedIn',
-    href: 'https://www.linkedin.com/in/codewithsadee'
+    href: 'https://www.linkedin.com/in/muaz-awais-a755412a6/'
   },
   {
     label: 'Twitter X',
-    href: 'https://x.com/codewithsadee_'
+    href: 'https://x.com/'
   },
   {
     label: 'Instagram',
-    href: 'https://www.instagram.com/codewithsadee'
-  },
-  {
-    label: 'CodePen',
-    href: 'https://codepen.io/codewithsadee'
+    href: 'https://www.instagram.com/muaz_awais'
   }
 ];
 
