f2dd8a93907ad57c5b95e657bc46b4cc9541c000
muazawais <muazawais1947@gmail.com>
2025-10-17 23:40:22 +0500

    protfolio posting3

--- src/homePage/work/index.jsx
+++ src/homePage/work/index.jsx
@@ -5,27 +5,27 @@ import ProjectCard from './projectCard';
 const works = [
   {
     imgSrc: 'https://res.cloudinary.com/dv8dtipj1/image/upload/v1760475579/Untitled_design_3_mtueht.png',
-    title: 'Complete Portfolio',
-    tags: ['React', 'Development'],
-    projectLink: 'https://github.com/MuazAwais/PorfolioSite.git'
+    title: 'Persnol Portfolio',
+    tags: ['React', 'Development','Tailwind'],
+    projectLink: 'https://porfolio-site-beta.vercel.app/'
   },
   {
     imgSrc: 'https://res.cloudinary.com/dv8dtipj1/image/upload/v1760475580/Untitled_design_2_kviy9i.png',
-    title: 'E-Learning For you page ',
-    tags: ['Shadcn', 'React', 'Api'],
-    projectLink: 'https://github.com/MuazAwais/LearnEngine.git'
+    title: 'E-Learning Site',
+    tags: ['Shadcn', 'React', 'Api','Tailwind'],
+    projectLink: 'https://learn-engine-fawn.vercel.app/'
   },
   {
     imgSrc: 'https://res.cloudinary.com/dv8dtipj1/image/upload/v1760475580/Untitled_design_1_qkldep.png',
     title: 'Car Bloging Site',
-    tags: ['Development', 'React'],
-    projectLink: 'https://github.com/MuazAwais/Car-Blogging-site.git'
+    tags: ['Development', 'React','Tailwind','Shadcn'],
+    projectLink: 'https://car-blogging-site.vercel.app/'
   },
   {
     imgSrc: 'https://res.cloudinary.com/dv8dtipj1/image/upload/v1760475580/Untitled_design_wevic7.png',
-    title: 'Bakrey website',
-    tags: ['Web-design', 'Development'],
-    projectLink: 'https://github.com/MuazAwais/my-project.git'
+    title: 'Bakery Site',
+    tags: ['Web-design', 'Development','HTML','CSS'],
+    projectLink: 'https://my-project-sage-five.vercel.app/'
   },
 ];
 
